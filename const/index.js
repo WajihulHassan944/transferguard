b59c0d7934d4f66a57cc802eb68c2ffa8cc75a6e
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2026-01-31 18:54:46 +0500

    updated code

--- const/index.js
+++ const/index.js
@@ -1,4 +1,5 @@
-export const baseUrl = "http://localhost:3000/api";
+export const baseUrl = "https://www.transferguard.eu/api";
 
 // http://localhost:3000/api
 // http://77.42.91.228:3000/api
+// https://www.transferguard.eu
